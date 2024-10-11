df5f4bb20bff7d4c7ccde4c877bd23c8333f84ab
Jarkó Richárd <jarkoricsi16@gmail.com>
2024-10-11 05:38:31 +0200

    deleted mess from the public/index.php

--- public/index.php
+++ public/index.php
@@ -4,20 +4,6 @@ require __DIR__.'/../vendor/autoload.php';
 
 use SosoRicsi\JWT\JWT;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
-$dotenv->load();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../')->load();
 
 JWT::setKey(env('SECRET_KEY'));
-
-$payload = [
-	'username' => "SosoRicsi"
-];
-
-print $token = JWT::encode($payload);
-
-print "\n";
-
-print "<pre>";
-print_r(JWT::decode($token));
-
-JWT::reset();
